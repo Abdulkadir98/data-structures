609f092c9fb9c1524ef5b177d727eb0445b82449
Abdul Kadir <ak98.narutokun@gmail.com>
2019-03-16 12:37:33 +0530

    Add merge sort

---
+++ src/sorting/MergeSortDemo.java
@@ -0,0 +1,14 @@
+package sorting;
+
+public class MergeSortDemo {
+
+    public static void main(String[] args) {
+     Comparable[] a = {'M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'};
+
+     MergeSort.sort(a);
+     for (Comparable i: a) {
+         System.out.println(i);
+     }
+
+    }
+}
