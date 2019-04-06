5afd8dc51bfca3f68f38c79d04d90fc51c40359f
Abdul Kadir <ak98.narutokun@gmail.com>
2019-04-06 11:51:56 +0530

    Add connected components algorithm for undirected graphs

---
+++ src/symboltable/graphs/undirectedgraph/ConnectedComponents.java
@@ -0,0 +1,51 @@
+package symboltable.graphs.undirectedgraph;
+
+
+/**
+ * A data type that helps find all the connected components in a graph using dfs.
+ * The type can be queried for connectivity in constant time
+ */
+public class ConnectedComponents {
+
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public ConnectedComponents(Graph graph) {
+
+        marked = new boolean[graph.V()];
+        id = new int[graph.V()];
+        count = 0;
+
+        for (int v=0; v<graph.V(); v++) {
+            if (!marked[v]) {
+                dfs(graph, v);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Graph graph, int v) {
+
+        marked[v] = true;
+        id[v] = count;  // vertices in the same call of dfs have the same id, i.e they belong to the same component
+
+        for (int w: graph.adj(v)) {
+            if (!marked[w])
+                dfs(graph, w);
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    public boolean connected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+}
--- src/symboltable/graphs/undirectedgraph/GraphDemo.java
+++ src/symboltable/graphs/undirectedgraph/GraphDemo.java
@@ -9,19 +9,32 @@ public class GraphDemo {
         graph.addEdge(0, 5);
         graph.addEdge(4, 3);
         graph.addEdge(0, 1);
+        graph.addEdge(9, 12);
         graph.addEdge(6, 4);
         graph.addEdge(5, 4);
         graph.addEdge(0, 2);
+        graph.addEdge(11, 12);
+        graph.addEdge(9, 10);
         graph.addEdge(0, 6);
+        graph.addEdge(7, 8);
+        graph.addEdge(9, 11);
         graph.addEdge(5, 3);
 
-      //DepthFirstPaths paths = new DepthFirstPaths(graph, 0);
-        BreadthFirstPaths paths = new BreadthFirstPaths(graph, 0);
-      System.out.println(paths.hasPathTo(3));
+        ConnectedComponents cc = new ConnectedComponents(graph);
+        System.out.println(cc.count());
+        System.out.println(cc.connected(0, 7));
+        System.out.println(cc.connected(7, 8));
+        System.out.println(cc.connected(9, 12));
+        System.out.println(cc.id(12));
 
-      for (int x: paths.pathTo(3)) {
-          System.out.print(x + " ");
-      }
+
+      //DepthFirstPaths paths = new DepthFirstPaths(graph, 0);
+//        BreadthFirstPaths paths = new BreadthFirstPaths(graph, 0);
+//      System.out.println(paths.hasPathTo(3));
+//
+//      for (int x: paths.pathTo(3)) {
+//          System.out.print(x + " ");
+//      }
 
     }
 }
